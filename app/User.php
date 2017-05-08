6737f854161eb6ba44af8fa73e804f9bc640f2ea
Humphrey Mugambi <mugagambi@gmail.com>
2017-05-08 19:45:19 +0300

    generate auth boilerplate

--- app/User.php
+++ app/User.php
@@ -10,12 +10,18 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     *The table associated with this model
+     * @var string
+     */
+    protected $table = 'tbl_users';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role'
     ];
 
     /**
