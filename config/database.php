cd3e8b0e219433bb218c014270675e7a794d74a1
Humphrey Mugambi <mugagambi@gmail.com>
2017-05-13 23:00:58 +0300

    Deploy file finished

--- config/database.php
+++ config/database.php
@@ -1,10 +1,4 @@
 <?php
-//config for heroku
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
 return [
 
     /*
